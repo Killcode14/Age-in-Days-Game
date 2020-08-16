84c333ce977ad84441badc14632567e0d1e58ba9
Killcode14 <42558785+Killcode14@users.noreply.github.com>
2020-08-16 08:41:26 +0000

    Add files via upload

---
+++ Age In Days/static/js/script.js
@@ -0,0 +1,15 @@
+// Challenge 1: Your Age in Days
+
+function ageInDays(){
+    let birthYear = prompt('What year were you born... Good friend?');
+    let ageInDayss = (2020 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are '+ ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
